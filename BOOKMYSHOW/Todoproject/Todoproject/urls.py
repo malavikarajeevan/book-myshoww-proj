a8e363cbb253d16f985bf589de2f28ab5b21930e
malavikarajeevan <malavikarajeevan85@gmail.com>
2024-01-17 12:25:27 +0530

    new file added

--- BOOKMYSHOW/Todoproject/Todoproject/urls.py
+++ BOOKMYSHOW/Todoproject/Todoproject/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('TodoApp.urls'))
+    path('',include('TodoApp.urls')),
+    path('auth/',include('userauth.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
---
+++ BOOKMYSHOW/Todoproject/userauth/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def registerUser(req):
+    if req.method=='POST':
+        fname=req.POST.get("fname","")
+        lname=req.POST.get("lname","")
+        email=req.POST.get("email","")
+        username=req.POST.get("username","")
+        password=req.POST.get("password","")
+        cpassword=req.POST.get("cpassword","")
+        print(fname,lname,email,username,password,cpassword)
+
+    return render(req, 'registeruser.html')
